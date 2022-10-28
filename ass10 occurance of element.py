1f59586b362f1fbf4d6c5a7e17e024951dabf8cb
Sharafunneesa <115632205+Sharafunneesa@users.noreply.github.com>
2022-10-28 11:29:34 +0530

    Update ass10 occurance of element.py

--- ass10 occurance of element.py
+++ ass10 occurance of element.py
@@ -1,7 +1,16 @@
 # count occurrence of an element in a list
+# case 1:-
 
 lis = list(input("enter the elements of list :").split())
 elem = input("enter the element to check :")
+count1 = 0
+for i in lis:
+    if i == elem:
+        count1 = count1 + 1
+print("the occurrence of element in list is :", count1)
+
+# case 2:-
+
 count = lis.count(elem)
 print("the occurrence of element in list is :", count)
 
