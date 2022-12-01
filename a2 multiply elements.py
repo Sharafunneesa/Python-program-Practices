e96239a989ec03c65ad6441ae8cf9e281042893e
Sharafunneesa <115632205+Sharafunneesa@users.noreply.github.com>
2022-12-01 15:03:35 +0530

    Update a2 multiply elements.py

--- a2 multiply elements.py
+++ a2 multiply elements.py
@@ -1,8 +1,15 @@
 # Python Multiply all numbers in the list
 
+#taking the element of list fro user and printing it
 lis = list(input("enter the elements :").split())
 print("the input list is :", lis)
+
+#taking one variable name mul and initializing with one 
 mul = 1
+
+#using for loop taking elements from list one by one and mltiply with mul and store in mul
 for elem in lis:
     mul = mul * int(elem)
+
+#printing the total multiply value     
 print("the multiply of all numbers in list is :", mul)
