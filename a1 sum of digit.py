c2e2f6dce648a65067cf9c6a6988e981079f2529
Sharafunneesa <115632205+Sharafunneesa@users.noreply.github.com>
2022-12-01 14:53:48 +0530

    Update a1 sum of digit.py

--- a1 sum of digit.py
+++ a1 sum of digit.py
@@ -1,11 +1,17 @@
 # Python Sum of number digits in List
-
+#taking the list element from user
 lis = list(input("enter the elements :").split())
 print("the input list is :", lis)
+
+#taking an empty list for saving output
 out = []
+
+#usin for loop to get the element from list one by one
 for elem in lis:
     sum = 0
+    #using for loop adding digits of one element and uppending into output list
     for digit in str(elem):
         sum = sum + int(digit)
     out.append(sum)
-print("the output list is :", out)
+#printing the output list
+ print("the output list is :", out)
